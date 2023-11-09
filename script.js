ac22cf1aeaade7e2b5d4693af3350435633cf368
mrmeepmeep <myachou42@gmail.com>
2023-11-09 18:42:40 +0100

    Added functionality to numbers and operators

--- script.js
+++ script.js
@@ -1,53 +1,76 @@
-const buttons = document.querySelectorAll("#btn")
-const display = document.querySelector(".screen")
-const equal = document.querySelector("#equalBtn")
-const clear = document.querySelector("#clearBtn")
-let output = ""
-let previousValue;
-let currentValue;
-let num1;
-let num2;
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
 
+let clear = document.querySelector("#clearBtn")
+let equal = document.querySelector("#equalBtn")
+let decimal = document.querySelector("#decimalBtn")
 
+let numbers = document.querySelectorAll("#numberBtn")
+let operators = document.querySelectorAll("#operatorBtn")
 
-buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
-        let currentValue = e.target.dataset.value
-        display.value += currentValue
-    })
+let previousScreen = document.querySelector(".previous")
+let currentScreen = document.querySelector(".current")
+
+numbers.forEach((number) => number.addEventListener('click', (e) => {
+    handleNumber(e.target.textContent)
+    currentScreen.textContent = currentValue
+}))
+operators.forEach((op) => op.addEventListener('click', (e) => {
+    handleOperator(e.target.textContent)
+    previousScreen.textContent = previousValue + ' ' + operator;
+    currentScreen.textContent = currentValue
+}))
+clear.addEventListener('click' , (e) => {
+    previousValue = ''
+    currentValue = ''
+    operator = ''
+    previousScreen.textContent = ''
+    currentScreen.textContent = ''
+})
+
+equal.addEventListener('click' , function(){
+    calculate()
+    previousScreen.textContent = ''
+    currentScreen.textContent = previousValue
 })
 
 
-function operate(){
-
+function handleNumber(num){
+   if (currentValue.length <= 10) {
+    currentValue += num;
+   }
 }
 
-
-
-/* function add(num1,num2){
-    console.log (num1 + num2)
-
-}
-add(3,9)
-
-function subtract(num1,num2){
-
-    console.log(num1 - num2)
-
-}
-subtract(2,10)
-
-function multiply(num1,num2){
-
-    console.log(num1 * num2)
-
+function handleOperator(op){
+  operator = op;
+  previousValue = currentValue
+  currentValue = ''
+    
 }
 
-multiply(4,4)
+function calculate(){
+    previousValue = Number(previousValue)
+    currentValue = Number(currentValue)
 
-function divide(num1,num2){
-    console.log(num1 / num2)
+    if (operator === '+'){
+        previousValue += currentValue;
+    } else if(operator === '-'){
+        previousValue -= currentValue
+    } else if (operator === "*"){
+        previousValue *= currentValue
+    } else if (operator === '/'){
+        previousValue /= currentValue
+    } else if (operator === '%') {
+        previousValue %= currentValue
+    }
+    previousValue = roundNumber(previousValue)
+    previousValue = previousValue.toString()
+    currentValue = previousValue.toString()
+    console.log(previousValue)
 }
 
-divide(10,2) */
+function roundNumber(num){
+    return Math.round(num * 1000 / 1000)
+}
